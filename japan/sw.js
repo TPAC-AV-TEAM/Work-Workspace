738a634a182c28b71bd33edf185cc6c34898cf6b
TPAC AV-TEAM <twalong@gmail.com>
2026-06-07 16:33:27 +0800

    Add files via upload

--- japan/sw.js
+++ japan/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nagoya-2026-v34';
+const CACHE_NAME = 'nagoya-2026-v35';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
