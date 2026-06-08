27f93415d8b33a4aa4ec8953240c2ce60ebccb12
TPAC AV-TEAM <twalong@gmail.com>
2026-06-08 16:41:44 +0800

    Update sw.js

--- japan/sw.js
+++ japan/sw.js
@@ -1,5 +1,11 @@
 const CACHE_NAME = 'nagoya-2026-v42';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
+const APP_SHELL = [
+  './', 
+  './index.html', 
+  './manifest.webmanifest',
+  './icon-192.svg',
+  './icon-512.svg'
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
